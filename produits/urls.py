35fa19a23259beae455bfeec56088804f83d2d55
nembotnd1u <nembotnd1u@etu.univ-lorraine.fr>
2024-06-29 13:52:47 +0200

    j'avais un problème dans ma classe views. En effet, je recupérais le prix en utilisant request.FILES au lieu de request.POST. Et après avoir corrigé ledit problème, au lieu de reprendre le remplissage du formulaire à 0 en rechargeant son URL depuis l'acceuil, je voulais relancer sa soumission, ce qui n'a pas, selon moi, permi à mettre à jour le truc. Voici le message d'erreur que j'avais: IntegrityError at /create-produit/ NOT NULL constraint failed: produits_produit.description Request Method:	POST Request URL:	http://127.0.0.1:8000/create-produit/ Django Version:	5.0.6 Exception Type:	IntegrityError Exception Value: NOT NULL constraint failed: produits_produit.description Exception Location:	C:\Users\loicn\Documents\Dev_perso\Formations\TutoDjango\venv\Lib\site-packages\django\db\backends\sqlite3\base.py, line 329, in execute Raised during:	produits.views.CreateProduct Python Executable:	C:\Users\loicn\Documents\Dev_perso\Formations\TutoDjango\venv\Scripts\python.exe Python Version:	3.11.0 Python Path: ['C:\\Users\\loicn\\Documents\\Dev_perso\\Formations\\TutoDjango',  'C:\\Users\\loicn\\AppData\\Local\\Programs\\Python\\Python311\\python311.zip',  'C:\\Users\\loicn\\AppData\\Local\\Programs\\Python\\Python311\\DLLs',  'C:\\Users\\loicn\\AppData\\Local\\Programs\\Python\\Python311\\Lib',  'C:\\Users\\loicn\\AppData\\Local\\Programs\\Python\\Python311',  'C:\\Users\\loicn\\Documents\\Dev_perso\\Formations\\TutoDjango\\venv',  'C:\\Users\\loicn\\Documents\\Dev_perso\\Formations\\TutoDjango\\venv\\Lib\\site-packages'] Server time:	Sat, 29 Jun 2024 11:42:13 +0000"""

--- produits/urls.py
+++ produits/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import CreateProduct, index
+
+app_name    = 'produits'
 
 urlpatterns = [
     path('', index, name = 'index'),
+    path('create-produit/', CreateProduct.as_view(),name='create_product'),
 ]
--- produits/views.py
+++ produits/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import HttpResponse, render
+from django.views import View
+
+from .models import Produit
 
 
 # Create your views here.
 def index(request, *args, **Kwargs) : 
-    return render(request, 'index.html', {})
+    liste_produits = Produit.objects.all()
+    context  ={
+        'liste_produits' : liste_produits,
+        'nom' : "Produits de la boutique de Nembutsu",
+    }
+    return render(request, 'index.html', context)
+
+class CreateProduct(View):
+    def get(self, request, *args, **Kwargs):
+        return render( request, 'produits/create_product.html')
+    
+    def post(self, request, *args, **Kwargs):
+        nom = request.POST.get('nom')
+        description = request.POST.get('description')
+        prix = request.POST.get('prix')
+        image = request.FILES.get('image')
+        
+        produit = Produit(nom = nom, description = description, prix= prix, image = image) 
+        produit.save()
+        # ou j'aurais pu faire  : produit = Produit.objects.create(nom = nom, etc...)
+        if produit : 
+            return HttpResponse('Produit enregistré avec succès')
+        else : 
+            return HttpResponse("Erreur lors de l'enregistrement du produit")
+        
+    
